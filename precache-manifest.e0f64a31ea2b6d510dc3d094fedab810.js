3a1fc99e649a76d5cea5a564e0ca6d194f5a9e8d
Francisco <hidalgo_fco520@outlook.com>
2019-02-10 14:12:59 -0600

    deploy app with react js

---
+++ precache-manifest.e0f64a31ea2b6d510dc3d094fedab810.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "552d461b7ce330716456",
+    "url": "/static/js/main.552d461b.chunk.js"
+  },
+  {
+    "revision": "1a9113a44045e775ede3",
+    "url": "/static/js/1.1a9113a4.chunk.js"
+  },
+  {
+    "revision": "552d461b7ce330716456",
+    "url": "/static/css/main.1bc58c84.chunk.css"
+  },
+  {
+    "revision": "8b371682165924765cd0cc67394c0192",
+    "url": "/index.html"
+  }
+];
